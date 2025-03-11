c6d05cbe4a379f28c0997c0dd9c92d9360ac4b8b
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2025-03-11 19:29:43 +0200

    123

--- src/components/article1.js
+++ src/components/article1.js
@@ -6,7 +6,7 @@ function Article1() {
     <>
      <h1>Kas tā ir un vai tā ir struktūras vai uzvedības modeļa daļa?</h1>
 <a>Use Case diagramma ir uzvedības modeļa daļa, kas attēlo sistēmas funkcionalitāti un aktoru mijiedarbību ar sistēmu.</a>
-</>
+</>  
   );
   }
 export default Article1;
